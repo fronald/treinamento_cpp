ead8053885a42841df92adb158ad5479809ccd3b
andre <dlperussi@hotmail.com>
2016-10-25 09:57:07 -0200

    Teste da classe SalvaCalculo incluso.

--- test/TestSalvaCalculo.cpp
+++ test/TestSalvaCalculo.cpp
@@ -7,19 +7,30 @@
 #include <gtest/gtest.h>
 
 #include "SalvaCalculo.h"
+#include "Primos.h"
 
 class TestSalvaCalculo : public ::testing::Test {
     
 protected:
+    
+    Primos* primos;
+    SalvaCalculo* salvaCalculo;
+    
+    bool f;
+    
     virtual void SetUp() {
+        primos = new Primos;
+        salvaCalculo = new SalvaCalculo(primos);
         
+        salvaCalculo->salva("./" + primos->nome());
+        f = fopen(("./" + primos->nome()).c_str(), "r");
     }
     virtual void TearDown() {
-        
+        remove(("./" + primos->nome()).c_str());
+        delete salvaCalculo;
     }
 };
 
-TEST_F(TestSalvaCalculo, Teste) {
-    
-    
+TEST_F(TestSalvaCalculo, Teste) {  
+    EXPECT_TRUE(f);
 }
